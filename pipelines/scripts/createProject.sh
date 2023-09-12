de6bcbb7aafa84b8949143c99affbe2d342bc535
ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>
2023-09-12 16:16:19 +0530

    Update createProject.sh
    Signed-off-by: ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>

--- pipelines/scripts/createProject.sh
+++ pipelines/scripts/createProject.sh
@@ -61,7 +61,7 @@ if [ -z "$name" ];   then
     PROJECT_URL=${LOCAL_DEV_URL}/projects
     json='{ "name": "'${repoName}'", "description": "Created by Automated CI for feature branch"}'
     echo "Project url is ${PROJECT_URL}"
-    projectName=$(curl --location --request POST ${PROJECT_URL} \
+    projectName=$(curl --location --request POST '${PROJECT_URL}' \
     echo "Project name is ${projectName}"
     --header 'Content-Type: application/json' \
     --header 'Accept: application/json' \
