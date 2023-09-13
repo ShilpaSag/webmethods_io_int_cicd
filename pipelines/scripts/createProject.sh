7ab4f2d7c7b490f330498cc35c4e7b2bbc0facae
ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>
2023-09-13 12:32:30 +0530

    Update createProject.sh
    Signed-off-by: ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>

--- pipelines/scripts/createProject.sh
+++ pipelines/scripts/createProject.sh
@@ -63,7 +63,7 @@ if [ -z "$name" ];   then
     jsonString="'${json}'"
     echo "Project url is ${PROJECT_URL}"
     echo "json is ${jsonString}"
-    projectName=$"(curl --location --request POST ${PROJECT_URL} \
+    projectName=$"(curl --location --request POST '${PROJECT_URL}' \
     --header "Content-Type:application/json" \
     --header "Accept:application/json" \
     --data-raw '${json}' -u '${admin_user}':'${admin_password}')"
