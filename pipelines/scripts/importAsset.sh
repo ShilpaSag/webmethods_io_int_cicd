eec96976fa72ed0cd34893fca8f0169d85765afc
ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>
2023-09-13 17:19:43 +0530

    Update importAsset.sh
    Signed-off-by: ShilpaSag <143790121+ShilpaSag@users.noreply.github.com>

--- pipelines/scripts/importAsset.sh
+++ pipelines/scripts/importAsset.sh
@@ -74,7 +74,7 @@ debug=${@: -1}
   echo "AssetType:" $assetType
   if [[ $assetType = workflow* ]]; then
       FLOW_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/workflow-import
-      cd ${HOME_DIR}${repoName}/assets/workflows
+      cd "${HOME_DIR}${repoName}/assets/workflows"
       echo "Workflow Import:" ${FLOW_URL}
       echo $(ls -ltr)
   else
