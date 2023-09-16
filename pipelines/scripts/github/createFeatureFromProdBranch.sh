76a7fe81b50bb4c8838fcd20ede963c586f9d0a1
SanthoshKumarHN <98937583+SanthoshKumarHN@users.noreply.github.com>
2023-09-17 01:23:30 +0530

    Update createFeatureFromProdBranch.sh
    Signed-off-by: SanthoshKumarHN <98937583+SanthoshKumarHN@users.noreply.github.com>

--- pipelines/scripts/github/createFeatureFromProdBranch.sh
+++ pipelines/scripts/github/createFeatureFromProdBranch.sh
@@ -68,8 +68,8 @@ function echod(){
               git config user.email "noemail.com"
               git config user.name "${devUser}"
               git fetch --all
-          #   git checkout -b ${featureBranchName} origin/production
-              git branch ${featureBranchName} origin/production
+             git checkout -b ${featureBranchName} origin/production
+          #    git branch ${featureBranchName} origin/production
               git add .
               git commit -m "Synching from Prod for feature branch ${featureBranchName}"
               git remote -v
